1b3f956787bb0105c4502dd5d8b0860b9b17880f
Helior Colorado <me@helior.info>
2015-12-26 12:10:23 -0800

    Add drupal site-isms

---
+++ src/settings.php
@@ -0,0 +1,18 @@
+<?php
+$environment = getenv('SITE_ENV');
+$dir = __DIR__ . '/settings';
+
+include "$dir/settings.default.php";
+// Load default environment settings, if available.
+if (is_file("$dir/settings.$environment.default.php")) {
+  include "$dir/settings.$environment.default.php";
+}
+// Load private environment settings, if available.
+if (is_file("$dir/settings.$environment.php")) {
+  include "$dir/settings.$environment.php";
+}
+
+// Load local settings file for local workstations.
+if (is_file("$dir/settings.local.php")) {
+  include "$dir/settings.local.php";
+}
